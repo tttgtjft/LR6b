91dc27a9b16209bda2787bc852558c4bc81cacd9
tttgtjft <1onecov@gmail.com>
2022-04-10 17:21:57 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/LR6b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LR6b.dir/main.cpp.obj"
+  "LR6b.exe"
+  "LR6b.exe.manifest"
+  "LR6b.pdb"
+  "libLR6b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LR6b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
